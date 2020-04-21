2570e54939b398fca6f63cb38e01313dea061d8d
kooku19 <kooku190642@gmail.com>
2020-04-21 17:39:36 +0700

    covid-19 21/04/63

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 $(function () {
 
     var url = "https://pomber.github.io/covid19/timeseries.json";
-
     $.getJSON(url, function (result) {
 
         var no = 1;
@@ -11,18 +10,40 @@ $(function () {
                         <th scope="row">${no}</th>
                         <td>
                         <a href="country.html?country=${country}">${country}</a>
-                        </td>
+                        </td> 
                     </tr>`;
 
-            $("#data").append(row);
+            $("#data1").append(row);
             no++;
+        }
+    });
+    var urlParams = new URLSearchParams(window.location.search);
+    console.log(urlParams.get('country'));
+
+    var country = urlParams.get('country');
+    $("#country").html(country);
+
+    var url = "https://pomber.github.io/covid19/timeseries.json";
+
+    $.getJSON(url, function (result) {
+
+        var selectedCountry = result[country];
+        console.log(selectedCountry);
+
+        for(var i=0;i<selectedCountry.length;i++){
+           
+            var row = `<tr>
+            <th scope="row">${selectedCountry[i].date}</th>
+            <td>${selectedCountry[i].confirmed}</td>
+            <td>${selectedCountry[i].deaths}</td>
+            <td>${selectedCountry[i].recovered}</td>
+          </tr>`
+
+          $("#data2").append(row);
 
         }
+        
 
     });
 
-
-
-
-
-})
+});
